214cd9a65edfe2a897491a2d3ad3e841c07c5ef6
pswainuu <pswaindev@outlook.com>
2021-06-23 11:04:02 +0530

    4

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+<<<<<<< HEAD
         self.response.write('Hello, World3!')
+=======
+        self.response.write('Hello, World!')
+>>>>>>> parent of bb0add7 (4)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
